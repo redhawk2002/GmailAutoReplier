b2eb14e3c10031a57010886b1e4f7055b874d284
Sandeepan Kalita <77795912+redhawk2002@users.noreply.github.com>
2023-11-12 22:58:20 +0530

    Delete routes directory

--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const controllers = require("./controllers");
-const router = express.Router();
-
-router.get("/mail/read/:messageId", controllers.readMail);
-
-module.exports = router;
